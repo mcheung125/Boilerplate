7e1f8aad44f64413ff860b0e026c26016a324a2c
Michael Cheung <mcheung125@gmail.com>
2019-09-11 14:04:54 -0400

    Sequelize Complete

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const app = require('./server')
+const app = require('./server');
+const db = require('./server/db/db.js');
 
 // Server
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log("Knock, knock");
-    console.log("Who's there?");
-    console.log(`Your server, listening on port ${port}`);
-})
+const port = process.env.PORT || 3000;
+db.sync().then(() => {
+	app.listen(port, () => {
+		console.log('Knock, knock');
+		console.log("Who's there?");
+		console.log(`Your server, listening on port ${port}`);
+	});
+});
